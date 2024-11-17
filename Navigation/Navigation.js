96eddfd1e668c001f5e4153fac08c1d922481222
Contrapo <felipecarnauba033@gmail.com>
2024-11-16 21:25:01 -0300

    Commit 1

---
+++ Navigation/Navigation.js
@@ -0,0 +1,18 @@
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import { Text, View } from 'react-native'
+import React from 'react'
+import HomeScreen from '../screens/HomeScreen';
+
+export default function Navigation() {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+            </Stack.Navigator> 
+        </NavigationContainer>
+    )
+}
